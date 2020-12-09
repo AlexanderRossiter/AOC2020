0eca270fc1559f8050cffdc397e2606a766b6e07
ADR <alexrossiter28@gmail.com>
2020-12-09 17:57:37 +0000

    Now uses long.

--- src/main/java/com/alex/aoc2020/Day9.java
+++ src/main/java/com/alex/aoc2020/Day9.java
@@ -9,14 +9,15 @@ import java.util.List;
 public class Day9 {
     private static final InputGetter inputGetter = new InputGetter();
     private static final String PATH = "/Users/ADR/Documents/AOC2020/src/main/resources/inputs";
-    private static final List<Integer> input = inputGetter.getInputAsIntArrayList(9, PATH);
+    private static final List<Long> input = parseInput(inputGetter.getInputAsList(9, PATH));
+
 
     public static void part1() {
         System.out.println(findFirstInvalid());
     }
 
     public static void part2() {
-        List<Integer> contiguousSet = findContiguousSet(findFirstInvalid());
+        List<Long> contiguousSet = findContiguousSet(findFirstInvalid());
         System.out.println(Collections.max(contiguousSet) + Collections.min(contiguousSet));
     }
 
@@ -26,19 +27,28 @@ public class Day9 {
         part2();
     }
 
-    public static Integer findFirstInvalid() {
-        List<Integer> preceding25;
+    public static List<Long> parseInput(List<String> inputString) {
+        List<Long> inputLong = new ArrayList<>();
+        for (String s : inputString) {
+            inputLong.add(Long.parseLong(s));
+        }
+
+        return inputLong;
+    }
+
+    public static Long findFirstInvalid() {
+        List<Long> preceding25;
         for (int i = 25; i < input.size(); i++) {
-            preceding25 = new ArrayList<Integer>(input.subList(i-25,i));
+            preceding25 = new ArrayList<>(input.subList(i-25,i));
             if (!isValid(input.get(i), preceding25)) {
                 return input.get(i);
             }
         }
-        return -1;
+        return (long) -1;
 
     }
 
-    public static boolean isValid(int testInt, List<Integer> preceding25) {
+    public static boolean isValid(long testInt, List<Long> preceding25) {
         for (int i = 0; i < preceding25.size(); i++ ) {
             for (int j = i+1; j < preceding25.size(); j++) {
                 if (preceding25.get(i) + preceding25.get(j) == testInt) {
@@ -49,14 +59,14 @@ public class Day9 {
         return false;
     }
 
-    public static List<Integer> findContiguousSet(int desiredSum) {
-        List<Integer> contiguousSet;
+    public static List<Long> findContiguousSet(long desiredSum) {
+        List<Long> contiguousSet;
         for (int startingIndex = 0; startingIndex < input.size(); startingIndex++) {
-            int sum = 0;
+            long sum = 0;
             int loopIdx = startingIndex+1;
             while (sum <= desiredSum) {
-                contiguousSet = new ArrayList<Integer>(input.subList(startingIndex, loopIdx+1));
-                sum = contiguousSet.stream().reduce(0, Integer::sum);
+                contiguousSet = new ArrayList<>(input.subList(startingIndex, loopIdx+1));
+                sum = contiguousSet.stream().reduce((long) 0, Long::sum);
                 if (sum == desiredSum) {
                     return contiguousSet;
                 }
