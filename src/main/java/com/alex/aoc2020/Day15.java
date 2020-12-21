af012bd80cdec2dfc83d345b78f4344d4a54104b
ADR <alexrossiter28@gmail.com>
2020-12-21 15:37:24 +0000

    Day 15 complete.

---
+++ src/main/java/com/alex/aoc2020/Day15.java
@@ -0,0 +1,71 @@
+package com.alex.aoc2020;
+
+import com.alex.aoc2020.util.InputGetter;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Day15 {
+    private static final InputGetter inputGetter = new InputGetter();
+    private static final String PATH = "/Users/ADR/Documents/AOC2020/src/main/resources/inputs";
+    private static final List<Integer> input = Arrays.stream(inputGetter.getInputAsList(15, PATH).get(0).split(",")).map(Integer::parseInt).collect(Collectors.toList());
+    private static HashMap<Integer, List<Integer>> lastTimeSpokenMap;
+
+    public static void part1() {
+        System.out.println(playMemoryGame(2020));
+    }
+
+    public static void part2() {
+        System.out.println(playMemoryGame(30000000));
+    }
+
+    public static void main(String[] args) {
+        initialiseHashMap();
+        part1();
+        initialiseHashMap();
+        part2();
+    }
+
+
+    private static int playMemoryGame(int noRounds) {
+        int lastSpoken = input.get(input.size()-1);
+        int thisNumber;
+        for (int i = input.size(); i < noRounds; i++) {
+            if (numberPreviouslySpoken(lastSpoken)) {
+                thisNumber = getDifferenceBetweenRepeatedNumbers(lastSpoken);
+            } else {
+                thisNumber = 0;
+            }
+            lastSpoken = thisNumber;
+            addNumberToHashMap(thisNumber, i);
+        }
+        return lastSpoken;
+    }
+
+    private static void addNumberToHashMap(int n, int idx) {
+        if (lastTimeSpokenMap.containsKey(n)) {
+            List<Integer> arr = lastTimeSpokenMap.get(n);
+            arr.add(idx);
+            lastTimeSpokenMap.put(n, arr);
+        } else {
+            lastTimeSpokenMap.put(n, new ArrayList<>(Collections.singletonList(idx)));
+        }
+    }
+
+    private static void initialiseHashMap() {
+        lastTimeSpokenMap = new HashMap<>();
+        for (int i = 0; i < input.size(); i++) {
+            addNumberToHashMap(input.get(i), i);
+        }
+    }
+
+    private static boolean numberPreviouslySpoken(int n) {
+        return (lastTimeSpokenMap.containsKey(n) && lastTimeSpokenMap.get(n).size() > 1);
+    }
+
+    private static int getDifferenceBetweenRepeatedNumbers(int n) {
+        List<Integer> l = lastTimeSpokenMap.get(n);
+        return l.get(l.size()-1) - l.get(l.size()-2);
+    }
+
+}
