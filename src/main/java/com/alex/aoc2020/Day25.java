11811bfa889c28dc4c1aec184c0e7873338f5c59
ADR <alexrossiter28@gmail.com>
2021-01-07 21:13:11 +0000

    Day 25 complete.

---
+++ src/main/java/com/alex/aoc2020/Day25.java
@@ -0,0 +1,45 @@
+package com.alex.aoc2020;
+
+import com.alex.aoc2020.util.InputGetter;
+
+import java.util.List;
+
+public class Day25 {
+    private static final List<String> input = new InputGetter().getInputAsList(25,
+            "/Users/ADR/Documents/AOC2020/src/main/resources/inputs",
+            "\n");
+    private static long cardPublicKey = Long.parseLong(input.get(0));
+    private static long doorPublicKey = Long.parseLong(input.get(1));
+
+    public static void part1() {
+        System.out.println(getEncryptedKey( getLoopSize(7, cardPublicKey), doorPublicKey ));
+        System.out.println(getEncryptedKey( getLoopSize(7, doorPublicKey), cardPublicKey ));
+
+    }
+
+    public static void main(String[] args) {
+        part1();
+    }
+
+    private static int getLoopSize(long subjectNumber, long key) {
+        long testVal = 1;
+        int loopNumber = 0;
+        while (testVal != key) {
+            testVal *= subjectNumber;
+            testVal = testVal % 20201227;
+            loopNumber++;
+        }
+
+        return loopNumber;
+    }
+
+    private static long getEncryptedKey(int loopSize, long subjectNumber) {
+        long val = 1;
+        for (int i = 0; i < loopSize; i++) {
+            val *= subjectNumber;
+            val = val % 20201227;
+        }
+        return val;
+    }
+
+}
